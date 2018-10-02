b2d451879df5e8381f3ca9ffe2b70ccaa0d2fb03
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-10-02 19:24:00 +0200

    home

--- backend/assets/js/editar-contenido.js
+++ backend/assets/js/editar-contenido.js
@@ -172,12 +172,14 @@ app.controller('myCtrl', function ($scope) {
             // PAKO - DEFLATE FILE
             // https://github.com/nodeca/pako
             var pako = window.pako;
+            // Para usar pako.deflate, debemos indicarlo en putObject el atributo ContentEncoding con el valor deflate
             var htmlData = pako.deflate(html);
             // var htmlData = html; // no hacemos deflate: NO MUESTRA BIEN EL HTML EN PÁGINAS PRIVADAS (en modo incognito)
             
             var today = new Date();
             var nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
             
+            // Para usar pako.deflate, debemos indicarlo en el objeto subido con el atributo ContentEncoding con el valor deflate
             var paramsHtmlObject = { Bucket: $scope.bucket, Key: keyHTML, Body: htmlData, ContentType: "text/html", ContentEncoding: "deflate", Expires: nextweek};
             // var paramsHtmlObject = { Bucket: $scope.bucket, Key: keyHTML, Body: htmlData, ContentType: "text/html", ContentEncoding: "", Expires: nextweek};
             s3.putObject(paramsHtmlObject, function (errSavingFile, dataPutObject) {
