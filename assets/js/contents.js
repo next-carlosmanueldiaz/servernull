0b02e7a44fbd7cc588ab7283d55b723c3a3a5ed7
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-10-04 02:59:45 +0200

    home

--- assets/js/contents.js
+++ assets/js/contents.js
@@ -58,7 +58,7 @@ function expiredToken() {
  * @returns {undefined}
  */
 function signOut(e) {
-  e.preventDefault();
+  // e.preventDefault();
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
