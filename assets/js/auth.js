3c80227e716cfc656bcb8fef317fab2753aaaf3a
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-10-05 19:08:38 +0200

    home

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -166,7 +166,7 @@ function onLogIn(googleUser) {
                           sessionStorage.expireTime = rolAsumido.Credentials.Expiration;
                           sessionStorage.expired = false
                           sessionStorage.counter = 2;
-                          window.location.replace("/backend/index.html"); // Redirect anulado al backend.. mostramos home con login hecho
+                          // window.location.replace("/backend/index.html"); // Redirect anulado al backend.. mostramos home con login hecho
                         } else {
                           if (debug) console.log('Sorry! No Web Storage support..');
                         }
