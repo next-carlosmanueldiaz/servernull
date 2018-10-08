47ce8955a1ff40c312120a53f58b1786787aee7d
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-10-08 19:27:03 +0200

    home

--- assets/js/contents.js
+++ assets/js/contents.js
@@ -16,50 +16,56 @@ function getCredentials() {
       };
       var creds = new AWS.Credentials(credsData);
       AWS.config.update({region: region, credentials: creds});
+      if (debug) console.log('========================================');
+      if (debug) console.log('Credenciales:');
+      if (debug) console.log(AWS.config.credentials);
+      if (debug) console.log('RoleSessionName: ' + AWS.config.credentials.params.RoleSessionName);
       if (debug) console.log('Acceso condecido como administrador.');
       return true;
-    } else {
-      // Unauthenticated Identities
-      // ===========================================================================
-      // Obtenemos el rol de usuario no autenticado.
-      // https://docs.aws.amazon.com/es_es/cognito/latest/developerguide/switching-identities.html
-      // set the default config object
-      var creds = new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: IdentityPoolId
-      });
-      AWS.config.credentials = creds;
-      AWS.config.region = sessionStorage.region;
+    } 
+    // No es necesario hacer nada cuando el usuario es anónimo.
 
-      // Actualizamos y refrescamos
-      creds.expired = true;
-      AWS.config.update({ region: sessionStorage.region, credentials: creds });
-      AWS.config.credentials.refresh((errorRefreshCredentials) => {
-        if (errorRefreshCredentials) {
-          if (debug) console.log("error al refrescar las credenciales:");
-          if (debug) console.log(errorRefreshCredentials);
-        } else {
-          if (debug) console.log('Successfully logged on amazon after UPDATE & REFRESH!');
-          if (debug) console.log('Estas son las credenciales y refrescadas:');
-          if (debug) console.log('Region: ' + AWS.config.region);
-          if (debug) console.log('TOMAMOS POR DEFECTO EL ROL DEL INVITADO:');
-          if (debug) console.log('========================================');
-          if (debug) console.log('Credenciales:');
-          if (debug) console.log(AWS.config.credentials);
-          if (debug) console.log('RoleSessionName: ' + AWS.config.credentials.params.RoleSessionName)
-          if (debug) console.log('========================================');
-          if (debug) console.log('Almacenamos en sesión:');
-          sessionStorage.accessKeyId = AWS.config.credentials.accessKeyId; 
-          sessionStorage.secretAccessKey = AWS.config.credentials.secretAccessKey;
-          sessionStorage.sessionToken = AWS.config.credentials.sessionToken;
-          sessionStorage.expireTime = AWS.config.credentials.expireTime;
-          sessionStorage.expired = false
-          sessionStorage.counter = 2;
-          sessionStorage.rol = "invitado"
-          if (debug) console.log(sessionStorage);
-          return true;
-        }
-      });
-    }
+    // else {
+    //   // Unauthenticated Identities
+    //   // ===========================================================================
+    //   // Obtenemos el rol de usuario no autenticado.
+    //   // https://docs.aws.amazon.com/es_es/cognito/latest/developerguide/switching-identities.html
+    //   // set the default config object
+    //   var creds = new AWS.CognitoIdentityCredentials({
+    //       IdentityPoolId: IdentityPoolId
+    //   });
+    //   AWS.config.credentials = creds;
+    //   AWS.config.region = sessionStorage.region;
+
+    //   // Actualizamos y refrescamos
+    //   creds.expired = true;
+    //   AWS.config.update({ region: sessionStorage.region, credentials: creds });
+    //   AWS.config.credentials.refresh((errorRefreshCredentials) => {
+    //     if (errorRefreshCredentials) {
+    //       if (debug) console.log("error al refrescar las credenciales:");
+    //       if (debug) console.log(errorRefreshCredentials);
+    //     } else {
+    //       if (debug) console.log('Successfully logged on amazon after UPDATE & REFRESH!');
+    //       if (debug) console.log('Estas son las credenciales y refrescadas:');
+    //       if (debug) console.log('TOMAMOS POR DEFECTO EL ROL DEL INVITADO:');
+    //       if (debug) console.log('========================================');
+    //       if (debug) console.log('Credenciales:');
+    //       if (debug) console.log(AWS.config.credentials);
+    //       if (debug) console.log('RoleSessionName: ' + AWS.config.credentials.params.RoleSessionName);
+    //       if (debug) console.log('========================================');
+    //       if (debug) console.log('Almacenamos en sesión:');
+    //       sessionStorage.accessKeyId = AWS.config.credentials.accessKeyId; 
+    //       sessionStorage.secretAccessKey = AWS.config.credentials.secretAccessKey;
+    //       sessionStorage.sessionToken = AWS.config.credentials.sessionToken;
+    //       sessionStorage.expireTime = AWS.config.credentials.expireTime;
+    //       sessionStorage.expired = false
+    //       sessionStorage.counter = 2;
+    //       sessionStorage.rol = "invitado"
+    //       if (debug) console.log(sessionStorage);
+    //       return true;
+    //     }
+    //   });
+    // }
   } else {
     if (debug) console.log('No sessionStorage allowed.');
   }
