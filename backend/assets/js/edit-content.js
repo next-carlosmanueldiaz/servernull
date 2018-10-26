bfaf8e1ae46f8795c173f115ca8b9761e062e5f0
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-10-27 00:56:43 +0200

    edit content

--- backend/assets/js/edit-content.js
+++ backend/assets/js/edit-content.js
@@ -166,7 +166,7 @@ app.controller('myCtrl', function ($scope) {
             html = html.replace("{{script}}", "\r\n" + $scope.cts[$scope.pos].js);
             html = html.replace("{{css}}", "\r\n" + $scope.cts[$scope.pos].css);
             
-            titulo = $scope.cts[$scope.pos].fields[0].value;
+            titulo = $scope.content[0].value;
 
             var tpl = $scope.cts[$scope.pos].tpl;
             for (var key in $scope.cts[$scope.pos].fields) {
