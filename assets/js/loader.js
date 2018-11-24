7a0a93889cdcc43000c00210ac5e1a9d0e87403d
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-11-24 11:26:02 +0100

    loader

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -26,7 +26,7 @@ var idGoogleGapi = "googleGapi";
 
 var urls = scripts.urls;
 
-for (var i = 0; i < urls.length; i+=1) {
+for (var i = 0; i <= urls.length; i++) {
     var script = document.createElement('script');
     script.type = 'text/javascript';
     // script.async = 'async';
