78b462e3f1c1bf4a4c13a8d675b020516729f90b
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-11-24 18:04:20 +0100

    loader

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -7,8 +7,8 @@
 var js = {
     "librerias": [
         // No dependientes entre si
+        "https://cdnjs.cloudflare.com/ajax/libs/bootstrap.native/2.0.24/bootstrap-native.min.js",
         "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js",
         "https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js",
         "https://cdn.rawgit.com/aws/aws-sdk-js/master/dist/aws-sdk.js",
         "https://cdn.rawgit.com/aws/amazon-cognito-identity-js/master/dist/aws-cognito-sdk.min.js",
