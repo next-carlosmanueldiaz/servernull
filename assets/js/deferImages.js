48993d0692a6a67eb703da31e1c7b99359cbd41a
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-11-26 12:18:56 +0100

    bgImg

--- assets/js/deferImages.js
+++ assets/js/deferImages.js
@@ -1,62 +1,53 @@
 /**
- * 
+ * Toma todas las etiquetas de imágenes con clase deferload
+ * https://codepen.io/NickMcBurney/pen/RagyeW?editors=1010
  */
-// DEFER IMAGES FUNCTION
-function deferImages(){
-	// get all images with 'deferload' class
+
+/**
+ * DEFER IMAGES TAG FUNCTION
+ * Toma todos los tags de imagen <img> y les asigna el src una vez finalizada la carga de la página.
+ * Con esto conseguimos diferir la carga de las imagenes de la página.
+ * La carga de imágenes no impide la carga de la página (se cargan en paralelo)
+ */
+function deferImg(){
+	var debug = true;
+	// Toma todas las imagenes con la clase 'deferload'
 	var $images = document.querySelectorAll("img.deferload");
-	
-	// if there are images on the page run through each and update src
+	// Si hay imagenes en la pagina, ejecuta cada una y actualiza su src.
 	if($images.length > 0) {
-		for (var i = 0, len = $images.length; i < len; i++) {
-			
-			// get url from each image
+		for (var i = 0, len = $images.length; i < len; i++) {		
+			// Obtenemos la url de cada imagen desde el atributo data-src
 			var image_url = $images[i].getAttribute("data-src");
-			// set image src 
-			$images[i].src = image_url;
-			
+			// Establecemos el src
+			$images[i].src = image_url;		
 			// debugging
 			var $lognumber = i + 1;
-			console.log("Image No." + $lognumber + " loaded");
+			if (debug) console.log("Image No." + $lognumber + " loaded");
 		}
 	}
-	console.log("All Images loaded");
+	if (debug) console.log("All Images loaded");
 }
-
 
 // PAGE LOADED
 window.addEventListener('load', function() {
-	// run defer image function
-   deferImages();
+	// Ejecuta la función deferImg()
+   deferImg();
 }, false);
 
+//===============================================================================
+//===============================================================================
 
-// JQUERY
-// LAZY LOAD FUNCTION
-/*
-function lazyLoadImages(){
-	// get all images with 'lazyload' class
-	var $images = $(".deferload");
-	
-	// if there are images on the page run through each and update src
-	if($images.length > 0) {
-		$($images).each(function(i) {
-			var image_url = $(this).attr("data-delaysrc");
-			$(this).prop("src", image_url)
-			
-			// debugging
-			var $lognumber = i + 1;
-			console.log("Image No." + $lognumber + " loaded");
-		});
-	}
-	console.log("Images loaded")
+/**
+ * Difiere todas las propiedades CSS background-image
+ * 
+ */
+
+function deferBackgroundImage() {
+  var imgDefer = document.querySelectorAll('div[data-src]');
+  var style = "background-image: url({url})";
+  for (var i = 0; i < imgDefer.length; i++) {
+    imgDefer[i].setAttribute('style', style.replace("{url}", imgDefer[i].getAttribute('data-src')));
+  }
 }
+window.onload = deferBackgroundImage();
 
-
-// PAGE LOADED
-$(window).load(function(){  
-	console.log("Page loaded");
-	// run lazy load function
-   lazyLoadImages();  
-});
-*/
