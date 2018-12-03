a68732082d015d319ebaca20811cd6cdafc09114
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2018-12-03 13:43:53 +0100

    cabecera home

--- assets/js/interacciones.js
+++ assets/js/interacciones.js
@@ -16,66 +16,66 @@
 
 
     // SCROLL CABECERA
-    $(document).ready(function () {
-        if (!stickyEnabled) {
-            return;
-        }
+    // $(document).ready(function () {
+    //     if (!stickyEnabled) {
+    //         return;
+    //     }
 
-        var stickyFrom = window.stickyFrom;
+    //     var stickyFrom = window.stickyFrom;
 
-        var header = $('.header-main');
-        //var headerHeight = header.height();
-        var headerHeight = 153;
+    //     var header = $('.header-main');
+    //     //var headerHeight = header.height();
+    //     var headerHeight = 153;
 
-        if (!stickyFrom) {
-            stickyFrom = header.attr('data-sticky-from');
-        }
+    //     if (!stickyFrom) {
+    //         stickyFrom = header.attr('data-sticky-from');
+    //     }
 
-        if (stickyFrom && stickyFrom.match(/^\d+(\s*px)?$/)) {
-            stickyFrom = parseInt(stickyFrom.replace(/\s*px/g, ''));
+    //     if (stickyFrom && stickyFrom.match(/^\d+(\s*px)?$/)) {
+    //         stickyFrom = parseInt(stickyFrom.replace(/\s*px/g, ''));
 
-        } else {
-            var el = $(stickyFrom);
+    //     } else {
+    //         var el = $(stickyFrom);
 
-            if (el.length > 0) {
-                stickyFrom = el.offset().top + el.outerHeight(false) + 40;
-            }
-        }
+    //         if (el.length > 0) {
+    //             stickyFrom = el.offset().top + el.outerHeight(false) + 40;
+    //         }
+    //     }
 
-        if (!stickyFrom) {
-            stickyFrom = headerHeight;
-        }
+    //     if (!stickyFrom) {
+    //         stickyFrom = headerHeight;
+    //     }
 
-        //var totalHeader = $('header').height() + 20;
-        var totalHeader = headerHeight + 20;
-        var cintillo = $('.cintillo');
-        if (cintillo.length > 0) totalHeader = totalHeader + cintillo.height() + 10;
+    //     //var totalHeader = $('header').height() + 20;
+    //     var totalHeader = headerHeight + 20;
+    //     var cintillo = $('.cintillo');
+    //     if (cintillo.length > 0) totalHeader = totalHeader + cintillo.height() + 10;
 
-        stickyFrom = Math.max(50, Math.min($(document).height() - $(window).height() - headerHeight, stickyFrom), totalHeader);
+    //     stickyFrom = Math.max(50, Math.min($(document).height() - $(window).height() - headerHeight, stickyFrom), totalHeader);
 
-        var checkScroll = function () {
-            if (!stickyEnabled) {
-                return;
-            }
+    //     var checkScroll = function () {
+    //         if (!stickyEnabled) {
+    //             return;
+    //         }
 
-            var sticky = $('.header-main'),
-                scroll = $(window).scrollTop();
+    //         var sticky = $('.header-main'),
+    //             scroll = $(window).scrollTop();
 
-            if (scroll >= stickyFrom) {
-                sticky.addClass('fixed');
-                $('body').css('padding-top', headerHeight + "px");
-                $(window).trigger('resize');
+    //         if (scroll >= stickyFrom) {
+    //             sticky.addClass('fixed');
+    //             $('body').css('padding-top', headerHeight + "px");
+    //             $(window).trigger('resize');
 
-            } else {
-                sticky.removeClass('fixed');
-                $('body').css('padding-top', '0px');
-                $(window).trigger('resize');
-            }
-        };
+    //         } else {
+    //             sticky.removeClass('fixed');
+    //             $('body').css('padding-top', '0px');
+    //             $(window).trigger('resize');
+    //         }
+    //     };
 
-        $(window).scroll(_.throttle(checkScroll, defaultThrottle));
-        checkScroll();
-    });
+    //     $(window).scroll(_.throttle(checkScroll, defaultThrottle));
+    //     checkScroll();
+    // });
 
     // DOCUMENT-READY
     $(document).ready(function () {     
