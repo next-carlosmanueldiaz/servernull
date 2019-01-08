a78bf883b8012f37be9b71a44ea4178c0e74b545
Carlos Manuel Diaz <carlosmanuel.diaz.jorge@bbva.com>
2019-01-08 09:00:31 +0100

    edit home

---
+++ backend/assets/js/edit-home.js
@@ -0,0 +1,31 @@
+
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+
+  this.$onInit = function () {
+    const permisos = getAccess(); // auth.js
+    $scope.bucket = bucket; // config.js
+
+    const keyHome = 'index.html';
+    var fileParams = {Bucket: $scope.bucket, Key: keyHome};
+    var reqGetIndex = new AWS.S3().getObject(fileParams, function (errGetObject, fileDataContentTypes) {});
+    // create the promise object
+    var promiseGetIndex = reqGetIndex.promise();
+
+    // Manejamos los estados completado/rechazado de la promesa
+    promiseGetIndex.then(
+      function(data) {
+        const id = getQueryVariable("id");
+        var content = JSON.parse(fileDataContentTypes.Body.toString('utf-8'));
+        $scope.html = content;
+        $scope.$apply();
+      },
+      function(errGetObject) {
+        if (debug) console.log('El fichero ' + key + ' NO existe en el bucket o no tiene permisos.');
+        if (debug) console.log(errGetObject);
+        expiredToken();
+      }
+    );
+  }
+});
